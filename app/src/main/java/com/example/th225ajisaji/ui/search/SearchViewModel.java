c29736b460e1284523eb944fcf801f127e26d6de
keihayashibe-note <zigu000@gmail.com>
2022-11-11 12:18:22 +0900

    r.id.buttonからfragment_oniku.xmlの画面遷移　テスト　動作

--- app/src/main/java/com/example/th225ajisaji/ui/search/SearchViewModel.java
+++ app/src/main/java/com/example/th225ajisaji/ui/search/SearchViewModel.java
@@ -10,7 +10,7 @@ public class SearchViewModel extends ViewModel {
 
     public SearchViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is SearchFragment fragment");
+        mText.setValue("123434565");
     }
 
     public LiveData<String> getText() {
