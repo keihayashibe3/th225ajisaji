18244ff237ceb7f76dbd049853db9a00a97982de
keihayashibe-home <zigu000@gmail.com>
2023-01-03 01:51:23 +0900

    エビチャーハン選択でSetImageResource

--- app/src/main/java/com/example/th225ajisaji/Ebichan.java
+++ app/src/main/java/com/example/th225ajisaji/Ebichan.java
@@ -1,15 +1,34 @@
 package com.example.th225ajisaji;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Ebichan extends AppCompatActivity {
+
+
+    public void onClickEbi2(View v) {
+int i =1;
+        ImageView Imagefav = (ImageView)this.findViewById(R.id.fav4);
+        if(i == 1){
+            i = 0;
+          Imagefav.setImageResource(R.drawable.ebityahan_sentaku);
+        }else {
+            i = 1;
+            Imagefav.setImageResource(R.drawable.ebityahan);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yasai);
-        setTitle( "野菜" );
+        setTitle( "選択エビ" );
     }
+
 }
--- app/src/main/java/com/example/th225ajisaji/yasai.java
+++ app/src/main/java/com/example/th225ajisaji/yasai.java
@@ -3,8 +3,24 @@ package com.example.th225ajisaji;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class yasai extends AppCompatActivity {
+
+int i = 0;
+    public void onClickEbi2(View v) {
+
+        ImageView Imagefav = (ImageView)this.findViewById(R.id.fav4);
+        if(i == 1){
+            i = 0;
+            Imagefav.setImageResource(R.drawable.ebityahan_sentaku);
+        }else {
+            i = 1;
+            Imagefav.setImageResource(R.drawable.ebityahan);
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
